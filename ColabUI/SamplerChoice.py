c35e5b0e7de19f201a1edc3a88a1213a42d70c40
yonathansantosa <yonathansantosa@gmail.com>
2024-06-12 07:46:54 +0700

    Fix indentation

--- ColabUI/SamplerChoice.py
+++ ColabUI/SamplerChoice.py
@@ -11,7 +11,7 @@ class SamplerChoice:
 
         self.dropdown = Dropdown(
             options=["Euler", "Euler A", "DPM++ 2M", "DPM++ 2M Karras", "DPM++ 2M SDE Karras", "DPM++ SDE", "DPM++ SDE Karras", "LMS", "LMS Karras", "UniPC"],
-            description='Samplers:',
+            description='Sampler:',
         )
         
         def dropdown_eventhandler(change):
@@ -21,37 +21,37 @@ class SamplerChoice:
 
         self.dropdown.observe(dropdown_eventhandler, names='value')
         
-        def choose_sampler(self, pipe, sampler_name: str):
-            config = pipe.scheduler.config
-            match sampler_name:
-                case "Euler": sampler = EulerDiscreteScheduler.from_config(config)
-                case "Euler A": sampler = EulerAncestralDiscreteScheduler.from_config(config)
-                case "DPM++ 2M": sampler = DPMSolverMultistepScheduler.from_config(config)
-                case "DPM++ 2M Karras":
-                    sampler = DPMSolverMultistepScheduler.from_config(config)
-                    sampler.use_karras_sigmas = True
-                case "DPM++ 2M SDE":
-                    sampler = DPMSolverMultistepScheduler.from_config(config)
-                    sampler.algorithm_type = "sde-dpmsolver++"
-                case "DPM++ 2M SDE Karras":
-                    sampler = DPMSolverMultistepScheduler.from_config(config)
-                    sampler.use_karras_sigmas = True
-                    sampler.algorithm_type = "sde-dpmsolver++"
-                case "DPM++ SDE":
-                    sampler = DPMSolverSinglestepScheduler.from_config(config)
-                case "DPM++ SDE Karras":
-                    sampler = DPMSolverSinglestepScheduler.from_config(config)
-                    sampler.use_karras_sigmas = True
-                case "LMS Karras":
-                    sampler = LMSDiscreteScheduler.from_config(config)
-                    sampler.use_karras_sigmas = True
-                case "LMS":
-                    sampler = LMSDiscreteScheduler.from_config(config)
-                case "UniPC":
-                    sampler = UniPCMultistepScheduler.from_config(config)
-                case _: raise NameError("Unknown sampler")
-            pipe.scheduler = sampler
-            print(f"Sampler '{sampler_name}' chosen")
+    def choose_sampler(self, pipe, sampler_name: str):
+        config = pipe.scheduler.config
+        match sampler_name:
+            case "Euler": sampler = EulerDiscreteScheduler.from_config(config)
+            case "Euler A": sampler = EulerAncestralDiscreteScheduler.from_config(config)
+            case "DPM++ 2M": sampler = DPMSolverMultistepScheduler.from_config(config)
+            case "DPM++ 2M Karras":
+                sampler = DPMSolverMultistepScheduler.from_config(config)
+                sampler.use_karras_sigmas = True
+            case "DPM++ 2M SDE":
+                sampler = DPMSolverMultistepScheduler.from_config(config)
+                sampler.algorithm_type = "sde-dpmsolver++"
+            case "DPM++ 2M SDE Karras":
+                sampler = DPMSolverMultistepScheduler.from_config(config)
+                sampler.use_karras_sigmas = True
+                sampler.algorithm_type = "sde-dpmsolver++"
+            case "DPM++ SDE":
+                sampler = DPMSolverSinglestepScheduler.from_config(config)
+            case "DPM++ SDE Karras":
+                sampler = DPMSolverSinglestepScheduler.from_config(config)
+                sampler.use_karras_sigmas = True
+            case "LMS Karras":
+                sampler = LMSDiscreteScheduler.from_config(config)
+                sampler.use_karras_sigmas = True
+            case "LMS":
+                sampler = LMSDiscreteScheduler.from_config(config)
+            case "UniPC":
+                sampler = UniPCMultistepScheduler.from_config(config)
+            case _: raise NameError("Unknown sampler")
+        pipe.scheduler = sampler
+        print(f"Sampler '{sampler_name}' chosen")
 
     @property
     def render_element(self): 
